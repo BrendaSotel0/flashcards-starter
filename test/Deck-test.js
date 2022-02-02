f9c99985386ebbb11661abb0f7c7d76a65e98c88
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-02-01 17:34:47 -0700

    Add test and solution for count cards function

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -5,9 +5,19 @@ const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
 
 describe('Deck', () => {
+  let card1;
+  let card2;
+  let card3;
   let deck;
 
   beforeEach(() => {
-    deck = new Deck();
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
   });
+
+  it('should have a method that counts how many cards are in the deck', () => {
+    expect(deck.countCards()).to.equal(3);
+  })
 })
